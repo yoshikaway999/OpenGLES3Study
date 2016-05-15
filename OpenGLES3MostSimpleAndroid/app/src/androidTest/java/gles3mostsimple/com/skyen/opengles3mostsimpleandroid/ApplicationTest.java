a63b5e43e252b421298de88c8da3d79089177899
skyen55 <sumiokajiwara5@gmail.com>
2016-05-15 23:07:27 +0900

    no message

---
+++ OpenGLES3MostSimpleAndroid/app/src/androidTest/java/gles3mostsimple/com/skyen/opengles3mostsimpleandroid/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gles3mostsimple.com.skyen.opengles3mostsimpleandroid;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
